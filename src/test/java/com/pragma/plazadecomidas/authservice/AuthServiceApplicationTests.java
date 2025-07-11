153f23ab5f8a8c33ca9b9ffb44ab4ecc2aaf7962
Leonel Borja <sambor9119@gmail.com>
2025-07-11 11:54:45 -0500

    Primer commit

---
+++ src/test/java/com/pragma/plazadecomidas/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.plazadecomidas.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
